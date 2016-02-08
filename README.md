aef80b0cc57ed0e01ac7e0af72dd89caaabeeaa1
Hannah Wolfe <erisds@gmail.com>
2016-02-08 16:17:38 +0000

    Initial version
    - Uses nconf for config management
    - `npm start` calls bin/www
    - server and cron are both started
    - run port, cron ping seconds and cron endpoint are all configurable

---
+++ README.md
@@ -0,0 +1,13 @@
+# Download Count Service
+
+Service to ping ghost.org to get the current download count, and return it to anyone who requests it.
+
+## Usage
+
+To start the service:
+
+`npm start`
+
+To start the service, with debug output:
+
+`DEBUG=count:* npm start`
---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var logger = require('morgan');
+
+var store = require('./lib/store');
+var app = express();
+
+app.use(logger('dev'));
+
+// Configure express
+app.set('x-powered-by', false);
+app.set('query parser', false);
+
+function corsHeaders(req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Request-Method', 'GET');
+    res.set('Cache-Control', 'no-cache');
+
+    next();
+}
+
+app.get('/', corsHeaders, function(req, res, next) {
+    res.status(200).json({'count': store.get() });
+});
+
+app.options('/', corsHeaders, function (req, res, next) {
+    res.sendStatus(204);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+// stacktraces only sent in development mode
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: app.get('env') === 'development' ? err : {}
+    });
+});
+
+module.exports = app;

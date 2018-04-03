2aa058b3f78253e6fd4d1bdd0bbb56ad70115983
Hannah Wolfe <erisds@gmail.com>
2018-04-03 12:43:38 +0100

    ⬆️  Upgrade Ghost-Ignition to 2.9.1
    - add updated logging configuration

--- logging.js
+++ logging.js
@@ -1,12 +1,18 @@
-var config = require('ghost-ignition').config();
+const config = require('ghost-ignition').config();
+const loggly = config.get('logging:loggly') || {};
 
-var logging = require('ghost-ignition').logging({
+// Set the match to critical unless config says otherwise.
+loggly.match = loggly.match || 'level:critical';
+
+// see defaults in GhostLogger
+const logging = require('ghost-ignition').logging({
     env: config.get('env'),
     path: config.get('logging:path'),
     domain: config.get('logging:domain'),
     mode: config.get('logging:mode'),
     level: config.get('logging:level'),
-    transports: config.get('logging:transports')
+    transports: config.get('logging:transports'),
+    loggly: loggly
 });
 
 module.exports = logging;
